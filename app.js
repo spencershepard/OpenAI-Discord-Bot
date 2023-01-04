20a8157878dddea8cb46ded1a096d91406255932
spencershepard <spencershepard@hotmail.com>
2023-01-04 12:44:47 -0800

    add more api error handling messages

--- app.js
+++ app.js
@@ -78,10 +78,19 @@ client.on('messageCreate', message => {
 
       if (error.response && error.response.data) {
         if ( error.response.data.error && error.response.data.error.message) {
-          if (running_conversation && error.response.data.error.message.startsWith("This model's maximum context length")) {
+          
+          if (error.response.data.error.message.startsWith("This model's maximum context length")) {
             // Tokens are a bit tricky to manage, so if we run out of tokens, just reset the conversation
-            message.channel.send("Sorry, could you please repeat that?");
+            message.channel.send("Hi! That other bot was tired, so I'm taking over. What do you want to talk about?");
             conversations[message.channel] = "";
+          }
+
+          if (error.response.data.error.message.startsWith("You have reached your")) {
+            message.channel.send("Sorry, I'm out of tokens for the day. Try again later!");
+          }
+          
+          if (error.response.data.error.message.startsWith("That model is currently overloaded")) {
+            message.channel.send("Sorry, I'm a bit bogged down right now. Try again in a few minutes!");
           }
         }
         console.log(error.response.data);
