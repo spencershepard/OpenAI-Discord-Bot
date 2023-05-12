f09e23dbcd9958e4c54da2e206497e32758ce8e2
spencershepard <spencershepard@hotmail.com>
2023-05-11 21:14:55 -0700

    axios brotli compression z_buf_error
    issue #6

--- openai_text.js
+++ openai_text.js
@@ -61,7 +61,8 @@ openai_text.sendPrompt = function (message) {
 
     const headers = {
         'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + openai_api_key
+        Authorization: 'Bearer ' + openai_api_key,
+        'Accept-Encoding': 'gzip,deflate,compress'
     };
 
     // Send the text to the OpenAI API and get the response
